bdd5e9e17a62d18e535dd21d857447908697feb6
Olexandr1996 <longervs@gmail.com>
2021-11-01 08:59:51 +0200

    One commit

---
+++ index.js
@@ -0,0 +1,42 @@
+//Task 1
+//let ask = prompt("Please put length square") 
+//alert(`Perimeter   -   ${ask * 4}`) // This action will correct by Visial Code.
+//Task 2 
+//let ask = +prompt("Please put length square");  
+//function Myfuction(ask) { 
+//if (ask % 2 ==0) 
+//{
+ //return alert( ask*8); 
+//}  
+//else 
+//{
+    //return alert (ask*9);
+//}
+   
+//}  
+//Myfuction(ask); 
+ //Task 3 
+ //function checkPalindrome(str) {
+
+    
+    //let len = string.length;
+
+    
+    //for (let i = 0; i < len / 2; i++) {
+
+        
+        //if (string[i] !== string[len - 1 - i]) {
+            //return 'It is not a palindrome';
+        //}
+    //}
+    //return 'It is a palindrome';
+//}
+
+
+//let string = prompt('Enter a string: ');
+
+
+//let value = checkPalindrome(string);
+
+//console.log(value); 
+
